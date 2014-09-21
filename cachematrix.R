ed341c602023d4f37a33faa077d0fa98dd96d159
tzuuyi <tzuuyi@yahoo.com>
2014-09-21 16:22:29 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+#makeCacheMatrix function is to get inverse of a matrix and save the data in 
+#an object.
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -5,21 +7,27 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setsol <- function(solve) m <<- solve 
+	  setsol <- function(solve) m <<- solve
         getsol <- function() m
         list(set = set, get = get,
              setsol = setsol,
              getsol = getsol)
 }
 
-cacheSolve <- function(x, ...) {
-        m <- x[x["getsol"]]
+#cacheSolve function is used to get inverse of a matrix
+#it will check first if the same matrix has its inverse 
+#already.  If not, it will use solve function to get it.
+cacheSolve <- function(x = matrix, ...) {
+	  #keep getting error $operator is invalid for atomic vector
+	  #but use x[x[...]] and getting NA instead, so function
+	  #that gets matrix is different?
+        m <- x$getsol()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- x[x["get"]]
+        data <- x$get()
         m <- solve(data, ...)
-        x[x["setsol(m)"]]
+        x$setsol(m)
         m
 }
